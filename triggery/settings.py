edb7615466085be9be285d599c171ba43b12cd79
Rajul Jha <rajuljha49@gmail.com>
2025-03-14 13:47:56 +0530

    Add render hostname to allowed hosts

--- triggery/settings.py
+++ triggery/settings.py
@@ -27,7 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
